4691fac7ec364b44f4aa6821b44dbecd159e528b
retarted <45270044+retarted@users.noreply.github.com>
2018-11-22 11:05:45 -0700

    Update tic tac toe.cpp

--- tic tac toe.cpp
+++ tic tac toe.cpp
@@ -9,13 +9,10 @@ Description(I.P.O):
 #include <iomanip>
 #include <string>
 
-using namespace std;
-     int board() {
-
     cout<<"                      x                             x                          "<<endl;
     cout<<"                      x                             x                          "<<endl;
     cout<<"                      x                             x                          "<<endl;
-    cout<<"           1          x               2             x           3              "<<endl;
+    cout<<"                      x               2             x         3                "<<endl;
     cout<<"                      x                             x                          "<<endl;
     cout<<"                      x                             x                          "<<endl;
     cout<<"                      x                             x                          "<<endl;
@@ -25,7 +22,7 @@ using namespace std;
     cout<<"                      x                             x                          "<<endl;
     cout<<"                      x                             x                          "<<endl;
     cout<<"                      x                             x                          "<<endl;
-    cout<<"            4         x              6              x           6              "<<endl;
+    cout<<"         4            x              6              x           6              "<<endl;
     cout<<"                      x                             x                          "<<endl;
     cout<<"                      x                             x                          "<<endl;
     cout<<"                      x                             x                          "<<endl;
@@ -33,51 +30,14 @@ using namespace std;
     cout<<"                      x                             x                          "<<endl;
     cout<<"                      x                             x                          "<<endl;
     cout<<"                      x                             x                          "<<endl;
-    cout<<"            7         x              8              x             9            "<<endl;
+    cout<<"        7             x         8                   x             9            "<<endl;
     cout<<"                      x                             x                          "<<endl;
     cout<<"                      x                             x                          "<<endl;
     cout<<"                      x                             x                          "<<endl;
     cout<<"                      x                             x                          "<<endl;
-     }
-int main(){
-
-
-    string play_one=" ";
-    string play_two=" ";
-    char playone_char=' ';
-    char playtwo_char=' ';
-    int player_move=0;
-
-
-    cout<<"Enter player one's name: ";
-    cin>>play_one;
-    cout<<"Enter player two's name: ";
-    cin>>play_two;
-
-    cout<<"Choose whether "<<play_one<<" wishes to use x's or o's: ";
-    cin>>playone_char;
-
-    if(playone_char=='x'){
-        cout<<play_two<<"'s character is o "<<endl;
-
-    }else if(playone_char=='o'){
-        cout<<play_two<<"'s character is x "<<endl;
-
-    }else if(playone_char!='x' || 'o'){
-        cout<<"ERROR. PLEASE SELECT EITHER X OR O: ";
-        cin>>playone_char;
     }
 
-    cout<<play_one<<" is going first"<<endl;
-    board();
-    cout<<"Please choose your first move based on the numbered positions: ";
-    cin>>player_move;
 
-
-
-
-     return 0;
-     }
 
 
      int o(){
@@ -95,6 +55,63 @@ int main(){
      cout<<"  X X  ";
      cout<<"X    X ";
      }
+
+using namespace std;
+     int board() {
+
+    string play_one     ="";
+    string play_two     ="";
+    char playOne_char   =' ';
+    char playTwo_char   =' ';
+    int player_move     = 0;
+    int firstPlay       = 0;
+
+
+    cout<<"Enter player one's name: ";
+    cin>>play_one;
+    cout<<"Enter player two's name: ";
+    cin>>play_two;
+
+    cout<<"Choose whether "<<play_one<<" wishes to use x's or o's: ";
+    cin>>playOne_char;
+
+    playOne_char = tolower(playOne_char);
+
+    while (playOne_char != 'x' || playOne_char != 'o') {
+        cout << "Error invalid entry, the only playable characters are x and o, please re-enter: ";
+        cin  >> playOne_char;
+        playOne_char = tolower(playOne_char);
+
+    }
+
+    if(playOne_char=='x'){
+        cout<<play_two<<"'s character is o "<<endl;
+        playTwo_char = 'o';
+
+    }
+    else if(playOne_char=='o'){
+        cout<<play_two<<"'s character is x "<<endl;
+         playTwo_char = 'x';
+
+    }
+
+
+    cout << "Who is going first, " << play_one << "(1) or " << play_two << "(2)?";
+    cin  >> firstPlay;
+
+    while (firstPlay != 1 || firstPlay != 2) {
+    cout << "ERROR numbers 1 or 2 expected you entered: " << firstPlay << " please re-enter: ";
+    cin  >> firstPlay;
+    }
+
+
+    if ()
+    cout<<play_one<<" is going first"<<endl;
+    board();
+    cout<<"Please choose your first move based on the numbered positions: ";
+    cin>>player_move;
+
+}
 
 
 
